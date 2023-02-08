e513ab3f9981748048243ad63ef05b41dca3fd7e
Josue Ramirez <josue.ramirez.z@uni.pe>
2023-02-08 16:05:35 -0500

    feat:   - create image module   - create image migration   - fix multer config   - fix courses migration and schema

--- api/src/courses/course.module.ts
+++ api/src/courses/course.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './models/course.model';
 import { CoursesController } from './course.controller';
 import { CoursesService } from './course.service';
+import { ImgService } from 'src/images/img.service';
+import { ImageModule } from 'src/images/img.module';
 @Module({
   imports: [UserModule,TypeOrmModule.forFeature([
      Course ,
-    ])],
+    ]),ImageModule],
   controllers: [CoursesController],
-  providers: [CoursesService],
+  providers: [CoursesService,],
 })
 export class CoursesModule {}
--- api/src/migrations/1675535408025-Users.ts
+++ api/src/migrations/1675535408025-Users.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class api1675535408025 implements MigrationInterface {
-    name = '1675535408025'
+    name = '1675535408025Users'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "phoneNumber" character varying NOT NULL, "isAdmin" boolean NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_1e3d0240b49c40521aaeb953293" UNIQUE ("phoneNumber"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "firstname" character varying NOT NULL, "lastname" character varying NOT NULL, "phoneNumber" character varying NOT NULL, "isAdmin" boolean NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_1e3d0240b49c40521aaeb953293" UNIQUE ("phoneNumber"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
